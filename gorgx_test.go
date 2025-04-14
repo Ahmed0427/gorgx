7ca2ac871fcf832b73819fbd8a790e383faf784b
Ahmed0427 <ahmedabdallah1090@gmail.com>
2025-04-14 15:42:30 +0200

    change the test struct

--- gorgx_test.go
+++ gorgx_test.go
@@ -4,49 +4,29 @@ import (
 	"testing"
 )
 
-func validateEmail(email string) bool {
-	regex := `[a-zA-Z][a-zA-Z0-9_.]+@[a-zA-Z0-9]+(.[a-zA-Z]{2,})+`
-	tokens := parse(regex)
-    nfa := toNFA(tokens)
-	return match(nfa, email, 0)
+func validate(regex, input string) bool {
+	nfa := toNFA(parse(regex))
+	return match(nfa, input, 0)
 }
 
 func TestRegexEngine(t *testing.T) {
 	tests := []struct {
-		email    string
+		regex    string
+		input    string
 		expected bool
 	}{
-        // valid
-		{"test.email@example.com", true},
-		{"test_email123@domain.co", true},
-		{"john.doe123@company.io", true},
-		{"first_last@domain.name", true},
-		{"test.1_2_3@sub.domain.com", true},
-
-        // invalid
-		{"test.email@example.com", true},
-		{"@example.com", false},         
-		{"test@.com", false},           
-		{"test@domain", false},        
-		{"test@domain..com", false},  
-		{"test@domain.c", false},    
-		{"@domain.com", false},     
-		{"test@domain.123", false},
-		{"test@domain.-com", false},      
-		{"test@domain,com", false},      
-		{" test@email.com", false},     
-		{"test@email.com ", false},    
-		{" test@domain.com", false},  
-		{"test@domain.com ", false},      
+		{
+			regex: "([0-9]{4})/([0-9]{2})/([0-9]{2})",
+			input: "2025/04/14",
+			expected: true,
+		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.email, func(t *testing.T) {
-			result := validateEmail(test.email)
-			if result != test.expected {
-				t.Errorf("For email %s, expected %v, but got %v",
-                    test.email, test.expected, result)
-			}
-		})
+		result := validate(test.regex, test.input)
+		if result != test.expected {
+			t.Errorf("For email %s, expected %v, but got %v",
+				test.input, test.expected, result)
+		}
 	}
 }
